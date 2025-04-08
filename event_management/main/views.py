4e69d5475e18c59fa96f6546610bf47726d2466a
AbhiramReddy001 <abhiramreddyk007@gmail.com>
2025-04-08 15:03:39 +0100

    Update views.py

--- event_management/main/views.py
+++ event_management/main/views.py
@@ -10,14 +10,21 @@ def signup(request):
 
 def admin_signin(request):
     return render(request, 'admin_signin.html')
-
-
+    
+def dashboard(request):
+    return render(request, 'dashboard.html')
+    
+def create_profile(request):
+    return render(request, 'create_profile.html')
+    
 def signin(request):
     return render(request, 'signin.html')
    
 def admin_dashboard_view(request):
     return render(request, 'admin_dashboard.html')
-
+    
+def member_profile_settings_view(request):
+    return render(request, 'profile_settings.html')
 
 def admin_events_view(request):
     return render(request, 'admin_events.html')
