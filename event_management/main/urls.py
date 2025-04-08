b4a36a4ba24e34a39bad59e909a0211f23def8a5
SRINIVAS DATTU BOMMAKANTI <126263826+srinivasdattu@users.noreply.github.com>
2025-04-08 14:49:40 +0100

    Update urls.py

--- event_management/main/urls.py
+++ event_management/main/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('admin_create_event/', views.admin_create_event_view, name='admin_create_event'),
     path('member_events/', views.member_events_view, name='member_events'),
     path('digital_events/', views.digital_content_view, name='digital_events'),
+     path('admin_dashboard/', views.admin_dashboard_view, name='admin_dashboard'),
+    path('admin_members/', views.admin_members_view, name='admin_members'),
 ]
 
 if settings.DEBUG:
